b42b75fa62053a4c849eb65df1a8f45739b26861
Håkon Solbjørg <hakon.solbjorg@knowit.no>
2018-09-01 19:49:04 +0200

    source: Initial commit of source files.

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import "./app.scss";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+  </div>,
+  document.getElementById("app")
+);
