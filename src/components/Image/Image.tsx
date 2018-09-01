59d79a9d7ed373c1e013f7898630335be7e8682d
Ole Martin <oleknurvik@outlook.com>
2018-09-01 22:08:36 +0200

    Initial image files

---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./image.scss";
+
+const Image = () => (
+    <div>
+      <img src="https://www.w3schools.com/w3css/img_lights.jpg"/>
+    </div>
+);
+
+export default Image;
