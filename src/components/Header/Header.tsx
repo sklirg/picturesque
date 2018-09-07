6ddad07ee6ddfe22611d4957441e32ecdd4d5753
Ole Martin <oleknurvik@outlook.com>
2018-09-07 13:48:37 +0200

    Added header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+import "./header.scss";
+
+const Header = () => (
+  <div className="header">
+      <h1>Picturesque</h1>
+      <div className="headerMenu"><a href="">Login</a></div>
+    </div>
+);
+
+export default Header;
