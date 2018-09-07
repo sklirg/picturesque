130e894c6e979fab9c961e00aed63f8a0d64dd18
Ole Martin <oleknurvik@outlook.com>
2018-09-07 13:50:17 +0200

    Added album component

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -1,8 +1,27 @@
 import * as React from "react";
+
+import Image from "components/Image/Image";
+
 import "./album.scss";
 
-const Album = () => (
-      <div>hurrr</div>
-);
+interface IAlbum {
+  title: string;
+  tags: string[];
+  images: string[];
+}
+
+class Album extends React.Component < IAlbum, {}> {
+  public render() {
+    return (
+      <div className="albumContainer">
+        <h2>{this.props.title}</h2>
+        <div>{this.props.tags.map((tag) => <div className="tag">{tag}</div>)}</div>
+        <div className="pictureContainer">
+          {this.props.images.map((image) => <Image src={image}/>)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Album;
