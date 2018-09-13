cf7c830e7a83238f7dea8885dfbb1bc1c10ee387
Ole Martin <oleknurvik@outlook.com>
2018-09-13 18:17:31 +0200

    change App according to new components

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import Album from "components/Album/Album";
+import Header from "components/Header/Header";
+
 import "./app.scss";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World</h1>
+    <Header/>
+    <Album title="Album title" tags={["Landscape", "Travel"]}
+    images={["https://www.w3schools.com/w3css/img_lights.jpg",
+    "https://www.w3schools.com/w3css/img_lights.jpg",
+    "https://www.w3schools.com/w3css/img_lights.jpg",
+    "https://www.w3schools.com/w3css/img_lights.jpg",
+    "https://www.w3schools.com/w3css/img_lights.jpg"]} />
   </div>,
-  document.getElementById("app")
+  document.getElementById("app"),
 );
