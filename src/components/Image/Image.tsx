c45fab4c653ebae45c339b8c080280f7b122fb80
Ole Martin <oleknurvik@outlook.com>
2018-09-13 18:17:03 +0200

    create image component

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,10 +1,27 @@
 import * as React from "react";
 import "./image.scss";
 
-const Image = () => (
+interface IImage {
+  src: string;
+}
+/*
+class Image extends React.Component < IImage, {}> {
+  public render() {
+    return (
     <div>
-      <img src="https://www.w3schools.com/w3css/img_lights.jpg"/>
+      <img src={this.props.src}/>
     </div>
-);
+  );
+}*/
+
+class Image extends React.Component < IImage, {}> {
+  public render() {
+    return (
+      <div>
+        <img src="https://www.w3schools.com/w3css/img_lights.jpg"/>
+      </div>
+    );
+  }
+}
 
 export default Image;
